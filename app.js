cab4125adce9c8eebe4f898bca4c94ec977763d5
Ken Atopos <z1158229761@outlook.com>
2023-01-21 18:34:48 +0000

    start the project

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const blogs = [];
 
 const app = express();
 
@@ -15,13 +14,28 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.set('strictQuery', true);
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/blogDB')};
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeSC: homeStartingContent,
-    posts: blogs,
-  })
-})
+  Blog.find({}, (err, foundBlogs) => {
+    res.render("home", {
+      homeSC: homeStartingContent,
+      posts: foundBlogs
+      });
+  });
+});
 
 
 app.get("/about", (req, res) => {
@@ -42,30 +56,31 @@ app.get("/compose", (req, res) => {
 })
 
 
-app.get("/title/:titleName", (req, res) => {
-  blogs.forEach(blog => {
-    if (_.lowerCase(req.params.titleName) === _.lowerCase(blog.title)) {
-      res.render("post", {
-        postT: blog.title,
-        postB: blog.body,
-      });
-    }
-    }
-  );
+app.get("/blogs/:blogId", (req, res) => {
+  const requestedPostId = req.params.blogId;
+
+  Blog.findOne({_id: requestedPostId}, (err, blog) => {
+    res.render("post", {
+      postT: blog.title,
+      postB: blog.content,
+    })
+  })
 });
 
 
 app.post("/compose", (req, res) => {
   const newPostTitle = req.body.postTitle;
   const newPostBody = req.body.postArea;
-  const newContent = {
-    "title": newPostTitle,
-    "body": newPostBody,
-  }
-  blogs.push(newContent);
-  console.log(blogs);
-  res.redirect("/");
-})
+  const newBlog = new Blog ({
+    title: newPostTitle,
+    content: newPostBody,
+  });
+  newBlog.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
 
 
 app.listen(process.env.PORT || 3000, () => {
